46aa23af3ddff954e7ef46359cf0f2da90df0d78
MauricioZapata00 <maurozac123@gmail.com>
2022-04-03 13:35:27 -0500

    This is the configuration to allow all cross origins and all headers

--- src/main/java/ferreteria/raul/controllers/ProveedorsController.java
+++ src/main/java/ferreteria/raul/controllers/ProveedorsController.java
@@ -8,6 +8,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path = "/ferreteriaRaul")
 public class ProveedorsController {
 
